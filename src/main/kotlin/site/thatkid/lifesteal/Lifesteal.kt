a63eed7999bbf2403d9943347456b7427aa25114
ThatKid <thomas.cobiac@outlook.com>
2025-08-31 19:03:21 +0930

    Initial commit

---
+++ src/main/kotlin/site/thatkid/lifesteal/Lifesteal.kt
@@ -0,0 +1,14 @@
+package site.thatkid.lifesteal
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Lifesteal : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
